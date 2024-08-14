366785b60487b53264817366ce86c0f524613205
imverma09 <imverma45@gmail.com>
2024-08-14 22:47:34 +0530

    img change

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ function App() {
     { icon: '★', lang: "Mongo DB" },
   ]
   const project = [
-    { img : "3.jpg", link: "https://github.com/imverma09/multi-shop", title: 'Multi-shop (e-commerce)', lang: "React (single-page website)", date: "July 04 - Present" },
-    { img : "4.jpg", link: "https://github.com/imverma09/flip-game", title: 'Images-flip-Game', lang: "HTML/CSS, javaScript", date: "June 16 - July" },
-    { img : "2.jpg", link: "https://amsssmohammadpuriya.netlify.app/", title: 'School website Template', lang: "HTML/CSS, javaScript", date: "April 17 - May" },
-    { img : "1.jpg", link: "https://word-guess-game001.netlify.app/", title: 'Word-Guess-Game', lang: "HTML/CSS, javaScript", date: "May 6 - June" },
+    { img : "3.webp", link: "https://github.com/imverma09/multi-shop", title: 'Multi-shop (e-commerce)', lang: "React (single-page website)", date: "July 04 - Present" },
+    { img : "4.webp", link: "https://github.com/imverma09/flip-game", title: 'Images-flip-Game', lang: "HTML/CSS, javaScript", date: "June 16 - July" },
+    { img : "2.webp", link: "https://amsssmohammadpuriya.netlify.app/", title: 'School website Template', lang: "HTML/CSS, javaScript", date: "April 17 - May" },
+    { img : "1.webp", link: "https://word-guess-game001.netlify.app/", title: 'Word-Guess-Game', lang: "HTML/CSS, javaScript", date: "May 6 - June" },
   ]
   return (
     <>
